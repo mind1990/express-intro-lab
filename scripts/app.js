c5e9cb49dc5951228ae216d5146509f7ce84ad83
Tirapat <mind1990@gmail.com>
2018-10-02 09:09:01 -0700

    Monday Lab

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,22 @@
-console.log("Sanity Check: JS is working!");
-
 $(document).ready(() => {
 
-  // code in here
+	console.log("Sanity Check: JS is working!");
+
+
+	const handleSuccesAlbums = (json) => {
+		let container = $('.container');
+		for(let i = 0; i < json.lenght; i++) {
+
+		}
+	}; 
+
+
+  $.ajax({
+   method: 'GET',
+   url: 'http://localhost:3000/api/albums',
+   sucess: handleSuccesAlbums,
+   error: handleError
+ 	});
 
 });
+
